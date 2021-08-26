1e0efaa93b22498c993453e9a7dc67ca2b1aa95c
forgemes <forgemest@gmail.com>
2021-08-26 20:54:47 +0300

    Init app

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
-#encoding: utf-8
+# encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
+
+class Client < ActiveRecord::Base
+end
+
+class Barber < ActiveRecord::Base
+end
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
